0836088c41cc36b03a50c68c99b0f50aa6ba0b24
Tugcan  Ozer <tucibeyin@hotmail.com>
2025-10-05 00:51:01 +0300

    Update build.grandle.kts
    Güncellemeler.

--- Android/build.grandle.kts
+++ Android/build.grandle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.tucibeyin.aracabak"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
